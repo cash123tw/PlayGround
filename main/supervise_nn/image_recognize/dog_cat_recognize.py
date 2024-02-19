909fe3e5e8c89e6b5422de602db306ab8da01e9b
dk-turtle <cash123tw@gmail.com>
2024-02-19 18:41:12 +0800

    增加圖表顯示

--- main/supervise_nn/image_recognize/dog_cat_recognize.py
+++ main/supervise_nn/image_recognize/dog_cat_recognize.py
@@ -6,6 +6,7 @@ import main.utils.downloader as downloader
 
 from tensorflow.keras.applications import vgg16, vgg19
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from main.utils.plot import make_img
 
 data_set_name = 'tongpython/cat-and-dog'
 data_set_dir = os.path.join('../../../data/dog_cat')
@@ -83,12 +84,16 @@ def dog_cat_main(model_path=None, load_model=False, lr=1e-4):
 
     early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.01, patience=5, mode='min')
 
-    model.fit(train_data, epochs=50, validation_data=valid_data, callbacks=[early_stop_callback])
+    record = model.fit(train_data, epochs=100, validation_data=valid_data, callbacks=[early_stop_callback])
+    history = record.history
+
+    make_img('track','loss','accuracy',history['loss'],history['accuracy'],
+             save_path=os.getcwd(),save_name='loss_accuracy.jpg',show=True)
 
     model.save(model_path)
 
 
 if __name__ == '__main__':
-    if not os.path.exists('data_set_dir'):
+    if not os.path.exists(data_set_dir):
         downloader.download_kaggle_file(data_set_name, data_set_dir)
-    dog_cat_main(os.path.join('../../../lib/dog_cat_classification.model'))
+    dog_cat_main(os.path.join('../../../lib/dog_cat_classification_model'))
